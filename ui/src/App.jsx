d7834f2c2b4bbd285921c2d0fa50664fd7f8d9d8
veyon26 <vigneshveyontech26@gmail.com>
2026-03-16 17:06:13 +0530

    name change

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>     
+          <h1>Veyon Tech</h1>     
         </div>   
       </section>
     </>
