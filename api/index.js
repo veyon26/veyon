a18c9dc2bb1a7ce8f7c35fd921cca82d3d97d837
veyon26 <vigneshveyontech26@gmail.com>
2026-03-18 17:21:27 +0530

    router

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-const db = require("./config/db");
-const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
 
-const billing = require("./router/Billing");
+// routes
+const billingRoutes = require("./router/billing");
 
-app.use("/api", billing);
+app.use("/api", billingRoutes);
+
+// server
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`RUNNING API ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
